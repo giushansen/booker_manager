6325baa4bc9c747aa5be39d38dbcee62472410e3
Guillaume <fourretg@gmail.com>
2014-10-31 16:12:04 +0800

    MVC complete using fixtures - Part 2

---
+++ app/adapters/application.js
@@ -0,0 +1,7 @@
+import DS from 'ember-data';
+
+export default DS.FixtureAdapter.extend();
+
+/*export default DS.LSAdapter.extend({
+  namespace: 'booker-manager'
+});*/
---
+++ app/models/table.js
@@ -0,0 +1,27 @@
+import DS from 'ember-data';
+
+var Table = DS.Model.extend({
+  number: DS.attr('number'),
+  seats: DS.attr('number'),
+  seatsMax: DS.attr('number'),
+  canJoin: DS.attr('array'),
+});
+
+Table.reopenClass({
+  FIXTURES: [
+    { id: 1, number: 1, seats: 4, seatsMax: 5, canJoin: [2]},
+    { id: 2, number: 2, seats: 4, seatsMax: 4, canJoin: [1,3] },
+    { id: 3, number: 3, seats: 2, seatsMax: 3, canJoin: [2,4] },
+    { id: 4, number: 4, seats: 2, seatsMax: 3, canJoin: [0] },
+    { id: 5, number: 5, seats: 2, seatsMax: 2, canJoin: [0] },
+    { id: 6, number: 6, seats: 2, seatsMax: 2, canJoin: [0] },
+    { id: 7, number: 7, seats: 2, seatsMax: 2, canJoin: [0] },
+    { id: 8, number: 8, seats: 2, seatsMax: 2, canJoin: [0] },
+    { id: 9, number: 9, seats: 3, seatsMax: 3, canJoin: [0] },
+    { id: 10, number: 10, seats: 2, seatsMax: 3, canJoin: [0] },
+    { id: 11, number: 21, seats: 4, seatsMax: 5, canJoin: [0] },
+    { id: 11, number: 22, seats: 4, seatsMax: 5, canJoin: [0] },
+  ]
+});
+
+export default Table;
