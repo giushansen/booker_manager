e97ea6ca5fec7f1502ac8daa9d1c13437d6cc426
Guillaume <fourretg@gmail.com>
2014-11-04 15:30:03 +0800

    Improve design, model relationship and upgrade

--- app/models/table.js
+++ app/models/table.js
@@ -5,15 +5,16 @@ var Table = DS.Model.extend({
   seats: DS.attr('number'),
   seatsMax: DS.attr('number'),
   canJoin: DS.attr('array'),
+  bills: DS.hasMany('bill', {async: true}),
 });
 
 Table.reopenClass({
   FIXTURES: [
-    { id: 1, number: 1, seats: 4, seatsMax: 5, canJoin: [2]},
+    { id: 1, number: 1, seats: 4, seatsMax: 5, canJoin: [2], bills: [1, 2]},
     { id: 2, number: 2, seats: 4, seatsMax: 4, canJoin: [1,3] },
     { id: 3, number: 3, seats: 2, seatsMax: 3, canJoin: [2,4] },
-    { id: 4, number: 4, seats: 2, seatsMax: 3, canJoin: [0] },
-    { id: 5, number: 5, seats: 2, seatsMax: 2, canJoin: [0] },
+    { id: 4, number: 4, seats: 2, seatsMax: 3, canJoin: [0], bills: [3] },
+    { id: 5, number: 5, seats: 2, seatsMax: 2, canJoin: [0], bills: [4] },
     { id: 6, number: 6, seats: 2, seatsMax: 2, canJoin: [0] },
     { id: 7, number: 7, seats: 2, seatsMax: 2, canJoin: [0] },
     { id: 8, number: 8, seats: 2, seatsMax: 2, canJoin: [0] },
