961b610ae2d98be7c604c074e5967f7c269dc774
Guillaume <fourretg@gmail.com>
2014-12-31 12:13:41 +0800

    Fix Typo

--- app/routes/services.js
+++ app/routes/services.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  redirect: function () {;
+  redirect: function () {
     //TODO dynamically choose today's most appropriate service
     //this.store.find('service', params['service_id'])
     this.transitionTo('service', 1);
